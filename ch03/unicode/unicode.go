178f3075dfcc4b5d52c2b0756883e5ca8401618f
Peter Olsen <pto@me.com>
2013-11-17 20:57:20 -0500

    more unicode

--- ch03/unicode/unicode.go
+++ ch03/unicode/unicode.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Jos\u00e9")
+	fmt.Println("\u212bngstrom")
+	fmt.Println("\u00c5 or A\u030a or \x41\xcc\x8a")
+	fmt.Println("\u212b" == "\xe2\x84\xab")
+	fmt.Println("A\u030a" == "\x41\xcc\x8a")
+	fmt.Printf("% [1]x %[1]v\n", "A\u030a")
+	fmt.Printf("% [1]x %[1]v\n", "\x41\xcc\x8a")
 }
