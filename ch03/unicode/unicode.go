d92a9b1a31e9f615e15612917bf43f022d3fb6a9
Peter Olsen <pto@me.com>
2013-11-18 20:53:56 -0500

    add small ae

--- ch03/unicode/unicode.go
+++ ch03/unicode/unicode.go
@@ -9,4 +9,12 @@ func main() {
 	fmt.Println("A\u030a" == "\x41\xcc\x8a")
 	fmt.Printf("% [1]x %[1]v\n", "A\u030a")
 	fmt.Printf("% [1]x %[1]v\n", "\x41\xcc\x8a")
+	fmt.Println()
+
+	aes := ""
+	for _, char := range []rune{'\u00e6', 0xe6, 0346, 230, '\xE6', '\u00e6'} {
+		fmt.Printf("[0x%X '%c'] ", char, char)
+		aes += string(char)
+	}
+	fmt.Println(aes)
 }
