9f6f097e1fcb06fb2bd4ea02ced74fee452180e0
Peter Olsen <pto@me.com>
2014-01-20 20:31:55 -0500

    more formatting

--- ch03/formatting/formatting.go
+++ ch03/formatting/formatting.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -16,8 +17,13 @@ func main() {
 
 	fmt.Println("|123456" + strings.Repeat("|123456789", 5) +
 		strings.Repeat("|1234567", 2) + "|")
-	fmt.Printf("|%b|%9b|%-9b|%09b|% 9b|% 9b|% 7b|% 7b|\n",
+	fmt.Printf("|%b|%9b|%-9b|%09b|% 9b|% 9b|%+7b|%+7b|\n",
 		37, 37, 37, 37, 37, -37, 37, -37)
+	fmt.Printf("|%o|%#o|%# 8o|%#+ 8o|%+08o|\n", 41, 41, 41, 41, -41)
+	i := 3931
+	fmt.Printf("|%x|%X|%8x|%08x|%#04X|0x%04X|\n", i, i, i, i, i, i)
+	i = 569
+	fmt.Printf("|$%d|$%06d|$%+06d|$%s|\n", i, i, i, Pad(i, 6, '*'))
 }
 
 func intForBool(b bool) int {
@@ -34,4 +40,13 @@ func parseBool(s string) bool {
 		fmt.Printf("cannot parse \"%s\"\n", s)
 	}
 	return val
+}
+
+func Pad(number, width int, pad rune) string {
+	s := fmt.Sprint(number)
+	gap := width - utf8.RuneCountInString(s)
+	if gap > 0 {
+		return strings.Repeat(string(pad), gap) + s
+	}
+	return s
 }
