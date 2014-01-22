48501d923dd0cc148b9fbbfe7b9e846b53183a47
Peter Olsen <pto@me.com>
2014-01-21 19:10:25 -0500

    formatting floating point

--- ch03/formatting/formatting.go
+++ ch03/formatting/formatting.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"unicode/utf8"
@@ -24,6 +25,13 @@ func main() {
 	fmt.Printf("|%x|%X|%8x|%08x|%#04X|0x%04X|\n", i, i, i, i, i, i)
 	i = 569
 	fmt.Printf("|$%d|$%06d|$%+06d|$%s|\n", i, i, i, Pad(i, 6, '*'))
+	fmt.Printf("%d %#04x %U '%c' %U\n", 0x3a6, 934, '\u03a6', '\U000003a6', 934)
+	fmt.Println()
+
+	fmt.Println(strings.Repeat("|12345678901234567890", 3) + "|")
+	for _, x := range []float64{-.258, 7194.84, -60897162.0218, 1.500089e-8} {
+		fmt.Printf("|%20.5e|%20.5f|%s|\n", x, x, Humanize(x, 20, 5, '*', ','))
+	}
 }
 
 func intForBool(b bool) int {
@@ -49,4 +57,26 @@ func Pad(number, width int, pad rune) string {
 		return strings.Repeat(string(pad), gap) + s
 	}
 	return s
+}
+
+func Humanize(amount float64, width, decimals int, pad, separator rune) string {
+	dollars, cents := math.Modf(amount)
+	whole := fmt.Sprintf("%+.0f", dollars)[1:]
+	fraction := ""
+	if decimals > 0 {
+		fraction = fmt.Sprintf("%+.*f", decimals, cents)[2:]
+	}
+	sep := string(separator)
+	for i := len(whole) - 3; i > 0; i -= 3 {
+		whole = whole[:i] + sep + whole[i:]
+	}
+	if amount < 0.0 {
+		whole = "-" + whole
+	}
+	number := whole + fraction
+	gap := width - utf8.RuneCountInString(number)
+	if gap > 0 {
+		return strings.Repeat(string(pad), gap) + number
+	}
+	return number
 }
