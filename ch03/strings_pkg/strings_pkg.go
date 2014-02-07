31c138c43cd3a0118083331e218b7b7a5af31ae8
Peter Olsen <pto@me.com>
2014-02-06 20:09:46 -0500

    adding strings_pkg

---
+++ ch03/strings_pkg/strings_pkg.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	names := "Niccolò•Noël•Geoffrey•Amélie••Turlough•José"
+	fmt.Print("|")
+	for _, name := range strings.Split(names, "•") {
+		fmt.Printf("%s|", name)
+	}
+	fmt.Print("\n|")
+	for _, name := range strings.SplitAfter(names, "•") {
+		fmt.Printf("%s|", name)
+	}
+	fmt.Println()
+
+	for _, record := range []string{"László Lajtha*1892*1963",
+		"Édouard Lalo\t1823\t1892", "José Ángel Lamas|1775|1814"} {
+		fmt.Println(strings.FieldsFunc(record, func(char rune) bool {
+			switch char {
+			case '\t', '*', '|':
+				return true
+			}
+			return false
+		}))
+	}
+}
