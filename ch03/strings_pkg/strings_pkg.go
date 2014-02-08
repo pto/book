c5afa2a1b441fc681250dcef1ec94c5d412d0e83
Peter Olsen <pto@me.com>
2014-02-07 20:34:06 -0500

    more strings

--- ch03/strings_pkg/strings_pkg.go
+++ ch03/strings_pkg/strings_pkg.go
@@ -17,8 +17,10 @@ func main() {
 	}
 	fmt.Println()
 
-	for _, record := range []string{"László Lajtha*1892*1963",
-		"Édouard Lalo\t1823\t1892", "José Ángel Lamas|1775|1814"} {
+	for _, record := range []string{
+		"László Lajtha*1892*1963",
+		"Édouard Lalo\t1823\t1892",
+		"José Ángel Lamas|1775|1814"} {
 		fmt.Println(strings.FieldsFunc(record, func(char rune) bool {
 			switch char {
 			case '\t', '*', '|':
@@ -27,4 +29,11 @@ func main() {
 			return false
 		}))
 	}
+	fmt.Println()
+
+	names = " Antônio\tAndré\tFriedrish\t\t\tJean\t\tÉlisabeth\tIsabella \t"
+	names = strings.Replace(names, "\t", " ", -1)
+	fmt.Printf("|%s|\n", names)
+	fmt.Println()
+
 }
