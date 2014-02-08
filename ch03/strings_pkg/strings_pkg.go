016cb8eb86ce79a38b843722f5b57d6e52c9523e
Peter Olsen <pto@me.com>
2014-02-08 09:58:09 -0500

    still more strings

--- ch03/strings_pkg/strings_pkg.go
+++ ch03/strings_pkg/strings_pkg.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
+	"time"
+	"unicode"
 )
+
+const LOOPS = 10
 
 func main() {
 	names := "Niccolò•Noël•Geoffrey•Amélie••Turlough•José"
@@ -31,9 +36,53 @@ func main() {
 	}
 	fmt.Println()
 
-	names = " Antônio\tAndré\tFriedrish\t\t\tJean\t\tÉlisabeth\tIsabella \t"
+	names = " Antônio\tAndré\tFriedrich\t\t\tJean\t\tÉlisabeth\tIsabella \t"
 	names = strings.Replace(names, "\t", " ", -1)
 	fmt.Printf("|%s|\n", names)
+	fmt.Println(strings.Fields(names))
+	fmt.Printf("|%s|\n", strings.Join(strings.Fields(names), " "))
+	fmt.Printf("|%s|\n", SimpleSimplifyWhitespace(names))
+	fmt.Printf("|%s|\n", SimplifyWhitespace(names))
 	fmt.Println()
 
+	start := time.Now()
+	for i := 0; i < LOOPS; i++ {
+		MySimplifyWhitespace(names)
+	}
+	fmt.Println("MySimplifyWhitespace duration:", time.Since(start))
+
+	start = time.Now()
+	for i := 0; i < LOOPS; i++ {
+		SimplifyWhitespace(names)
+	}
+	fmt.Println("SimplifyWhitespace duration:", time.Since(start))
+}
+
+func MySimplifyWhitespace(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+
+func SimpleSimplifyWhitespace(s string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(s)), " ")
+}
+
+func SimplifyWhitespace(s string) string {
+	var buffer bytes.Buffer
+	skip := true
+	for _, char := range s {
+		if unicode.IsSpace(char) {
+			if !skip {
+				buffer.WriteRune(' ')
+				skip = true
+			}
+		} else {
+			buffer.WriteRune(char)
+			skip = false
+		}
+	}
+	s = buffer.String()
+	if skip && len(s) > 0 {
+		s = s[:len(s)-1]
+	}
+	return s
 }
