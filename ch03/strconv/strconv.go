d520f220a9ed55e86e8c6f1200165dc44bc5a806
Peter Olsen <pto@me.com>
2014-02-09 20:14:02 -0500

    more strconv

--- ch03/strconv/strconv.go
+++ ch03/strconv/strconv.go
@@ -14,4 +14,11 @@ func main() {
 		}
 	}
 	fmt.Println()
+
+	x, err := strconv.ParseFloat("-99.7", 64)
+	fmt.Printf("%8T %6v %v\n", x, x, err)
+	y, err := strconv.ParseInt("71309", 10, 16)
+	fmt.Printf("%8T %6v %v\n", y, y, err)
+	z, err := strconv.Atoi("71309")
+	fmt.Printf("%8T %6v %v\n", z, z, err)
 }
