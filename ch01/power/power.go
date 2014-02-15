2604e28db80bc8d9ced7d20cd79ce0db2b9cdbf6
Peter Olsen <pto@me.com>
2014-02-14 22:17:42 -0500

    adding 1<<16 and types

--- ch01/power/power.go
+++ ch01/power/power.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("MaxUint16 is", math.MaxUint16)
-	fmt.Println("Pow(2,16) is", math.Pow(2,16))
+	fmt.Printf("MaxUint16 is %[1]d (%[1]T)\n", math.MaxUint16)
+	fmt.Printf("Pow(2,16) is %[1]g (%[1]T)\n", math.Pow(2, 16))
+	fmt.Printf("1<<16 is %[1]d (%[1]T)\n", 1<<16)
 }
